f419c4eb2ae62526d28abd8e842ff0f6d6fe3639
Shailesh Sharma <ssshailesh3@gmail.com>
2025-12-30 20:14:55 +0530

    code formatting

--- discord-backend/src/socketHandlers/addNewUserHandler.js
+++ discord-backend/src/socketHandlers/addNewUserHandler.js
@@ -1,12 +1,8 @@
 const { addNewConnectUser } = require("./../serverStore");
 
-
-
 const addNewUserHandler = (socket , io)=>{
     const userId = socket.user.userId;
     addNewConnectUser(socket.id , userId);
 }
-
-
 
 module.exports = addNewUserHandler;
