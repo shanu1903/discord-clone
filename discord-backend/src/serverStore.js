4be95e7dce1e96873af01af424c2a7b3abc88ebf
Shailesh Sharma <ssshailesh3@gmail.com>
2026-01-01 21:10:17 +0530

    adding user online funcationality

--- discord-backend/src/serverStore.js
+++ discord-backend/src/serverStore.js
@@ -1,43 +1,50 @@
 const connectUsers = new Map();
 let io = null;
 
-const addNewConnectUser = (socketId , userId)=>{
-    connectUsers.set(socketId , { userId });
-    console.log("new user connected");
-    console.log(connectUsers)
-}
+const addNewConnectUser = (socketId, userId) => {
+  connectUsers.set(socketId, { userId });
+  console.log("new user connected");
+  console.log(connectUsers);
+};
 
+const removeConnectUser = (socketId) => {
+  connectUsers.delete(socketId);
+  console.log("user disconnected");
+  console.log(connectUsers);
+};
 
-const removeConnectUser = (socketId)=>{
-    connectUsers.delete(socketId);
-    console.log("user disconnected");
-    console.log(connectUsers)
-} 
+const getActiveConnections = (userId) => {
+  const activeConnections = [];
+  connectUsers.forEach((value, key) => {
+    if (value.userId === userId) {
+      activeConnections.push(key);
+    }
+  });
 
-const getActiveConnections = (userId) => { 
-    const activeConnections = [];
-    connectUsers.forEach((value, key ) => { 
-        if (value.userId === userId) {
-          activeConnections.push(key);
-        }
-    })
+  return activeConnections;
+};
 
-    return activeConnections;
-}
+const getOnlineUsers = () => {
+  const onlineUsers = [];
+  connectUsers.forEach(({ userId }, socketId) => {
+    onlineUsers.push({ socketId, userId });
+  });
+  return onlineUsers;
+};
 
 const setSocketInstance = (ioInstance) => {
-    io = ioInstance;
-}
+  io = ioInstance;
+};
 
-const getSocketInstance = () => { 
-    return io;
-}
-
+const getSocketInstance = () => {
+  return io;
+};
 
 module.exports = {
-    addNewConnectUser,
-    removeConnectUser,
-    getActiveConnections,
-    setSocketInstance,
-    getSocketInstance
-}
+  addNewConnectUser,
+  removeConnectUser,
+  getActiveConnections,
+  setSocketInstance,
+  getSocketInstance,
+  getOnlineUsers,
+};
--- discord-backend/src/socketServer.js
+++ discord-backend/src/socketServer.js
@@ -1,27 +1,37 @@
 const authSocketMiddleware = require("./middlewares/authSocketMiddleware");
 const addNewUserHandler = require("./socketHandlers/addNewUserHandler");
 const disconnectUserHandler = require("./socketHandlers/disconnectUserHandler");
-const { setSocketInstance } = require("./serverStore");
+const { setSocketInstance, getOnlineUsers } = require("./serverStore");
 
-const registerServer = (server)=>{
-    const io = require("socket.io")(server , {
-        cors : { 
-            origin : "*",
-            methods : ["GET" , "POST"]
-        }
-    })
+const registerServer = (server) => {
+  const io = require("socket.io")(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
 
-    setSocketInstance(io);
-    io.use(authSocketMiddleware)
+  setSocketInstance(io);
+  io.use(authSocketMiddleware);
 
-    io.on("connection" , (socket)=>{
-        console.log("New client connected" , socket.id);
-        addNewUserHandler(socket , io);
-        socket.on("disconnect" , ()=>{
-            disconnectUserHandler(socket , io);
-        })
-    })
-}
+  const emitOnlineUsers = () => {
+    const onlineUsers = getOnlineUsers();
+    console.log("online-users", onlineUsers);
+    io.emit("online-users", { onlineUsers });
+  };
 
+  io.on("connection", (socket) => {
+    console.log("New client connected", socket.id);
+    addNewUserHandler(socket, io);
+    emitOnlineUsers();
+    socket.on("disconnect", () => {
+      disconnectUserHandler(socket, io);
+    });
+  });
+
+  setInterval(() => {
+    emitOnlineUsers();
+  }, 8000);
+};
 
 module.exports = registerServer;
